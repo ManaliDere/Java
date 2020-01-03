eedc80922927f37657c21a691347d259ee20f1d2
unknown <manalidere123@gmail.com>
2020-01-03 20:19:38 +0530

    first commit

---
+++ src/concepts/log4j.java
@@ -0,0 +1,22 @@
+package concepts;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class log4j {
+
+	public static Logger log = Logger.getLogger(log4j.class.getName());
+	public static void main(String[] args) {
+		
+		
+		//PropertyConfigurator.configure("C://Manali//Selenium//loggerfile.txt");
+		//log.debug("Debug");
+		log.info("Info");
+		//log.error("Error");
+		
+		System.out.println("info error");
+		
+		
+	}
+
+}
